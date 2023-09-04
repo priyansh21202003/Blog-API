95a60ece725cb71e6cadc6c8b6698ead3c1b4cda
priyansh21202003 <priyanshaanjane@gmail.com>
2023-09-04 12:15:06 +0530

    Second Commit

---
+++ src/app/Servers.js
@@ -0,0 +1,3 @@
+export const SIGNUP_API="http://localhost:8000/user/signup"
+export const LOGIN_API="http://localhost:8000/user/login"
+export const FORGOT_PASSWORD="http://localhost:8000/user/forgotpassword"
